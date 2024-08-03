646615790e547a779daf17d9ef8bfa1927c5d002
Jegatheesan Kavienan <kavienanj@gmail.com>
2024-08-03 22:32:56 +0530

    removed amplify backend

---
+++ src/app/amplify_config.ts
@@ -0,0 +1,28 @@
+import { ResourcesConfig } from "aws-amplify";
+
+export const amplifyConfig: ResourcesConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+      identityPoolId: "",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+};
